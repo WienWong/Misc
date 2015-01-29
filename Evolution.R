9548d17708ed6388b69c6c88eaf7479b2cf9e315
WienWong <wangzichengfu@gmail.com>
2015-01-29 06:08:04 +0100

    Create Evolution.R
    1st v

---
+++ Evolution.R
@@ -0,0 +1,49 @@
+
+# Final exam of Evolution (2014 Fall) of Beijing University 
+
+# Q28
+31 / 140
+# 0.2214286
+34 / 138
+# 0.2463768
+122 / 142
+# 0.8591549
+
+# Q31
+9 / 105
+# 0.08571429
+
+# Q32
+diff = 1 - 0.782
+Kaa <- -log(1 - diff)
+Kaa
+# 0.2459005
+
+# Q33
+kaa <- Kaa / 2 / 92.3e6
+kaa
+# 1.332072e-09
+
+# Q34
+Pd1 = 0.5
+Pd2 = 0.375
+Kaa1 <- -log(1 - Pd1)
+Kaa1
+# 0.6931472
+Kaa2 <- -log(1 - Pd2)
+Kaa2
+# 0.4700036
+
+kaa1 <- Kaa1 / 2 / 1e9
+kaa1
+# 3.465736e-10
+kaa2 <- Kaa2 / 2 / 5e8
+kaa2
+# 4.700036e-10
+
+# Q36 Hawai
+Kaa = 0.0076
+kaa = 1e-9
+T <- Kaa / 2 / kaa
+T
+# 380, 0000
